19750c81db97723689a232a549bffe6a1b9442c4
Yuya Watari <puropuro203@hotmail.com>
2018-06-30 06:42:27 +0000

    Change directory structure

--- Calc4Cpp/make.sh
+++
@@ -1,8 +0,0 @@
-LLVM_VERSION_SUFFIX=-7
-
-#DEBUG="-g -O0"
-RELEASE="-Ofast -DNDEBUG"
-FLAGS=${RELEASE}
-LLVM_CONFIG=`llvm-config${LLVM_VERSION_SUFFIX} --cxxflags --ldflags --libs --system-libs | sed "s/-Wno-maybe-uninitialized//g"`
-
-clang++${LLVM_VERSION_SUFFIX} ${LLVM_CONFIG} -std=c++17 -fexceptions ${FLAGS} ./*.cpp
