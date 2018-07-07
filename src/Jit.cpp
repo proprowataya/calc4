cdc2ed3bd82fa8681df7bc525d313052b6578c0b
Yuya Watari <puropuro203@hotmail.com>
2018-07-07 14:55:56 +0900

    Partially implemented JIT

--- src/Jit.cpp
+++ src/Jit.cpp
@@ -45,6 +45,7 @@ namespace {
     size_t IntegerBits = sizeof(TNumber) * 8;
 
     template<typename TNumber> void GenerateIR(const CompilationContext &context, const std::shared_ptr<Operator> &op, llvm::LLVMContext *llvmContext, llvm::Module *llvmModule);
+    struct GMPFunctions;
     template<typename TNumber> class IRGenerator;
 }
 
@@ -126,21 +127,67 @@ TNumber RunByJIT(const CompilationContext &context, const std::shared_ptr<Operat
 }
 
 namespace {
-    template<typename TNumber>
-    void GenerateIR(const CompilationContext &context, const std::shared_ptr<Operator> &op, llvm::LLVMContext *llvmContext, llvm::Module *llvmModule) {
+    struct GMPFunctionsBase {
+    protected:
+        llvm::LLVMContext *context;
+        llvm::Module *module;
+
+        GMPFunctionsBase() {}
+
+        GMPFunctionsBase(llvm::LLVMContext *context, llvm::Module *module)
+            : context(context), module(module) {}
+    };
+
+    struct GMPFunctions : protected GMPFunctionsBase {
         /* GMP Types */
         llvm::Type *mpzStructType =
             llvm::StructType::create(
-                { llvm::ArrayType::get(llvm::Type::getInt1Ty(*llvmContext), sizeof(__mpz_struct)) },
+                { llvm::ArrayType::get(llvm::Type::getInt1Ty(*context), sizeof(__mpz_struct)) },
                 "__mpz_struct");
         llvm::Type *mpzIntType = llvm::ArrayType::get(mpzStructType, 1);
         llvm::Type *mpzPtrType = llvm::PointerType::getUnqual(mpzStructType);
 
-        /* Set integer size during LLVM compilation */
-        llvm::Type *IntegerType =
-            std::is_same<TNumber, __mpz_struct *>::value
-            ? mpzPtrType
-            : llvm::Type::getIntNTy(*llvmContext, IntegerBits<TNumber>);
+        GMPFunctions() {}
+
+        GMPFunctions(llvm::LLVMContext *context, llvm::Module *module)
+            : GMPFunctionsBase(context, module) {}
+
+#define STR(X) #X
+#define DECLARE_FUNCTION(NAME, ...) \
+        llvm::Function *llvm_ ## NAME = llvm::Function::Create( \
+            llvm::FunctionType::get(llvm::Type::getVoidTy(*this->context), { __VA_ARGS__ }, false), \
+            llvm::Function::ExternalLinkage, STR(__g ## NAME)  , this->module)
+
+        DECLARE_FUNCTION(mpz_init, mpzPtrType);
+        DECLARE_FUNCTION(mpz_set, mpzPtrType, mpzPtrType);
+        DECLARE_FUNCTION(mpz_init_set, mpzPtrType, mpzPtrType);
+        DECLARE_FUNCTION(mpz_set_si, mpzPtrType, llvm::Type::getInt32Ty(*this->context));
+        DECLARE_FUNCTION(mpz_clear, mpzPtrType);
+
+        DECLARE_FUNCTION(mpz_add, mpzPtrType, mpzPtrType, mpzPtrType);
+        DECLARE_FUNCTION(mpz_add_ui, mpzPtrType, mpzPtrType, llvm::Type::getInt32Ty(*this->context));
+        DECLARE_FUNCTION(mpz_sub, mpzPtrType, mpzPtrType, mpzPtrType);
+        DECLARE_FUNCTION(mpz_mul, mpzPtrType, mpzPtrType, mpzPtrType);
+        DECLARE_FUNCTION(mpz_mul_si, mpzPtrType, mpzPtrType, llvm::Type::getInt32Ty(*this->context));
+        DECLARE_FUNCTION(mpz_tdiv_q, mpzPtrType, mpzPtrType, mpzPtrType);
+        DECLARE_FUNCTION(mpz_tdiv_r, mpzPtrType, mpzPtrType, mpzPtrType);
+
+        llvm::Function *mallocfunc = llvm::Function::Create(
+            llvm::FunctionType::get(llvm::Type::getInt64PtrTy(*this->context), { llvm::Type::getInt64Ty(*this->context) }, false),
+            llvm::Function::ExternalLinkage, "malloc", this->module);
+    };
+
+    template<typename TNumber>
+    void GenerateIR(const CompilationContext &context, const std::shared_ptr<Operator> &op, llvm::LLVMContext *llvmContext, llvm::Module *llvmModule) {
+        std::unique_ptr<GMPFunctions> gmp = nullptr;
+        llvm::Type *IntegerType;
+
+        if (std::is_same<TNumber, __mpz_struct *>::value) {
+            gmp.reset(new GMPFunctions(llvmContext, llvmModule));
+            IntegerType = gmp->mpzPtrType;
+        } else {
+            IntegerType = llvm::Type::getIntNTy(*llvmContext, IntegerBits<TNumber>);
+        }
 
         std::unordered_map<std::string, llvm::Function *> functionMap;
         for (auto it = context.UserDefinedOperatorBegin(); it != context.UserDefinedOperatorEnd(); it++) {
@@ -156,7 +203,7 @@ namespace {
         {
             llvm::BasicBlock *mainBlock = llvm::BasicBlock::Create(*llvmContext, EntryBlockName, mainFunc);
             llvm::IRBuilder<> builder(mainBlock);
-            IRGenerator<TNumber> generator(llvmModule, llvmContext, &builder, mainFunc, functionMap, mpzStructType, mpzIntType, mpzPtrType);
+            IRGenerator<TNumber> generator(llvmModule, llvmContext, &builder, mainFunc, functionMap, gmp.get());
             generator.BeginFunction();
             op->Accept(generator);
             generator.EndFunction();
@@ -168,7 +215,7 @@ namespace {
             llvm::BasicBlock *block = llvm::BasicBlock::Create(*llvmContext, EntryBlockName, function);
             llvm::IRBuilder<> builder(block);
 
-            IRGenerator<TNumber> generator(llvmModule, llvmContext, &builder, function, functionMap, mpzStructType, mpzIntType, mpzPtrType);
+            IRGenerator<TNumber> generator(llvmModule, llvmContext, &builder, function, functionMap, gmp.get());
             generator.BeginFunction();
             context.GetOperatorImplement(definition.GetName()).GetOperator()->Accept(generator);
             generator.EndFunction();
@@ -186,9 +233,7 @@ namespace {
         llvm::Value *value;
 
         /* GMP Types */
-        llvm::Type *mpzStructType;
-        llvm::Type *mpzIntType;
-        llvm::Type *mpzPtrType;
+        GMPFunctions *gmp;
 
         IRGeneratorBase(
             llvm::Module *module,
@@ -196,12 +241,9 @@ namespace {
             llvm::IRBuilder<> *builder,
             llvm::Function *function,
             const std::unordered_map<std::string, llvm::Function *> &functionMap,
-            llvm::Type *mpzStructType,
-            llvm::Type *mpzIntType,
-            llvm::Type *mpzPtrType)
+            GMPFunctions *gmp)
             :
-            module(module), context(context), builder(builder), function(function), functionMap(functionMap),
-            mpzStructType(mpzStructType), mpzIntType(mpzIntType), mpzPtrType(mpzPtrType) {}
+            module(module), context(context), builder(builder), function(function), functionMap(functionMap), gmp(gmp) {}
 
         virtual void BeginFunction() = 0;
         virtual void EndFunction() = 0;
@@ -322,14 +364,14 @@ namespace {
 
             llvm::BasicBlock *ifTrue = llvm::BasicBlock::Create(*this->context, "", this->function);
             llvm::IRBuilder<> ifTrueBuilder(ifTrue);
-            IRGenerator<TNumber> ifTrueGenerator(this->module, this->context, &ifTrueBuilder, this->function, this->functionMap, this->mpzStructType, this->mpzIntType, this->mpzPtrType);
+            IRGenerator<TNumber> ifTrueGenerator(this->module, this->context, &ifTrueBuilder, this->function, this->functionMap, this->gmp);
             op.GetIfTrue()->Accept(ifTrueGenerator);
             this->builder = ifTrueGenerator.builder;
             this->builder->CreateStore(ifTrueGenerator.value, temp);
 
             llvm::BasicBlock *ifFalse = llvm::BasicBlock::Create(*this->context, "", this->function);
             llvm::IRBuilder<> ifFalseBuilder(ifFalse);
-            IRGenerator<TNumber> ifFalseGenerator(this->module, this->context, &ifFalseBuilder, this->function, this->functionMap, this->mpzStructType, this->mpzIntType, this->mpzPtrType);
+            IRGenerator<TNumber> ifFalseGenerator(this->module, this->context, &ifFalseBuilder, this->function, this->functionMap, this->gmp);
             op.GetIfFalse()->Accept(ifFalseGenerator);
             this->builder = ifFalseGenerator.builder;
             this->builder->CreateStore(ifFalseGenerator.value, temp);
@@ -358,137 +400,134 @@ namespace {
     template<>
     class IRGenerator<__mpz_struct *> : public IRGeneratorBase<__mpz_struct *> {
     private:
-        /* GMP Functions */
-        llvm::Function *gmpinit = llvm::Function::Create(
-            llvm::FunctionType::get(llvm::Type::getVoidTy(*this->context), { mpzPtrType }, false),
-            llvm::Function::ExternalLinkage, "__gmpz_init", this->module);
-        llvm::Function *gmpset = llvm::Function::Create(
-            llvm::FunctionType::get(llvm::Type::getVoidTy(*this->context), { mpzPtrType, mpzPtrType }, false),
-            llvm::Function::ExternalLinkage, "__gmpz_set", this->module);
-
-        /* Other Functions */
-        llvm::Function *mallocfunc = llvm::Function::Create(
-            llvm::FunctionType::get(llvm::Type::getInt64PtrTy(*this->context), { llvm::Type::getInt64Ty(*this->context) }, false),
-            llvm::Function::ExternalLinkage, "malloc", this->module);
-
-        /* GMP Variables */
+        /* GMP Variable */
         llvm::Value *localValue;
 
     public:
         using IRGeneratorBase<__mpz_struct *>::IRGeneratorBase;
 
         virtual void BeginFunction() override {
-            localValue = this->builder->CreateAlloca(mpzIntType);
-            this->builder->CreateCall(gmpinit, { GetValuePtr() });
+            localValue = this->builder->CreateAlloca(gmp->mpzIntType);
+            this->builder->CreateCall(gmp->llvm_mpz_init, { GetValuePtr() });
         }
 
         virtual void EndFunction() override {
-            llvm::Value *alloced = this->builder->CreateCall(mallocfunc, { builder->getInt64(sizeof(__mpz_struct)) });
-            llvm::Value *casted = this->builder->CreateBitCast(alloced, mpzPtrType);
-            this->builder->CreateCall(gmpinit, casted);
-            this->builder->CreateCall(gmpset, { casted, GetValuePtr() });
+            llvm::Value *alloced = this->builder->CreateCall(gmp->mallocfunc, { builder->getInt64(sizeof(__mpz_struct)) });
+            llvm::Value *casted = this->builder->CreateBitCast(alloced, gmp->mpzPtrType);
+            this->builder->CreateCall(gmp->llvm_mpz_init_set, { casted, GetValuePtr() });
+            this->builder->CreateCall(gmp->llvm_mpz_clear, { GetValuePtr() });
             this->builder->CreateRet(casted);
         }
 
-        llvm::Value *GetValuePtr() const {
+        llvm::Value *GetValuePtr(llvm::Value *value = nullptr) const {
+            if (value == nullptr) {
+                value = localValue;
+            }
+
             return this->builder->CreateInBoundsGEP(
-                localValue, { this->builder->getInt64(0), this->builder->getInt64(0) });
+                value, { this->builder->getInt64(0), this->builder->getInt64(0) });
         }
 
         virtual void Visit(const ZeroOperator &op) override {
-            //this->value = this->builder->getIntN(IntegerBits<TNumber>, 0);
+            this->builder->CreateCall(gmp->llvm_mpz_set_si, { GetValuePtr(), this->builder->getInt32(0) });
         }
 
         virtual void Visit(const OperandOperator &op) override {
-            //auto it = this->function->arg_begin();
-            //for (int i = 0; i < op.GetIndex(); i++) {
-            //    ++it;
-            //}
+            auto it = this->function->arg_begin();
+            for (int i = 0; i < op.GetIndex(); i++) {
+                ++it;
+            }
 
-            //this->value = &*it;
+            this->builder->CreateCall(gmp->llvm_mpz_set, { GetValuePtr(), &*it });
         }
 
         virtual void Visit(const DefineOperator &op) override {
-            //this->value = this->builder->getIntN(IntegerBits<TNumber>, 0);
+            this->builder->CreateCall(gmp->llvm_mpz_set_si, { GetValuePtr(), this->builder->getInt32(0) });
         }
 
         virtual void Visit(const ParenthesisOperator &op) override {
-            //this->value = this->builder->getIntN(IntegerBits<TNumber>, 0);
-            //for (auto& item : op.GetOperators()) {
-            //    item->Accept(*this);
-            //}
+            this->builder->CreateCall(gmp->llvm_mpz_set_si, { GetValuePtr(), this->builder->getInt32(0) });
+            for (auto& item : op.GetOperators()) {
+                item->Accept(*this);
+            }
         }
 
         virtual void Visit(const DecimalOperator &op) override {
-            //op.GetOperand()->Accept(*this);
-            //auto operand = this->value;
-
-            //auto multed = this->builder->CreateMul(operand, this->builder->getIntN(IntegerBits<TNumber>, 10));
-            //this->value = this->builder->CreateAdd(multed, this->builder->getIntN(IntegerBits<TNumber>, op.GetValue()));
+            op.GetOperand()->Accept(*this);
+            this->builder->CreateCall(gmp->llvm_mpz_mul_si, { GetValuePtr(), GetValuePtr(), this->builder->getInt32(10) });
+            this->builder->CreateCall(gmp->llvm_mpz_add_ui, { GetValuePtr(), GetValuePtr(), this->builder->getInt32(op.GetValue()) });
         }
 
         virtual void Visit(const BinaryOperator &op) override {
-            //op.GetLeft()->Accept(*this);
-            //auto left = this->value;
-            //op.GetRight()->Accept(*this);
-            //auto right = this->value;
+            // Allocate temporal variable
+            llvm::Value *temp = this->builder->CreateAlloca(gmp->mpzIntType);
 
-            //switch (op.GetType()) {
-            //case BinaryType::Add:
-            //    this->value = this->builder->CreateAdd(left, right);
-            //    break;
-            //case BinaryType::Sub:
-            //    this->value = this->builder->CreateSub(left, right);
-            //    break;
-            //case BinaryType::Mult:
-            //    this->value = this->builder->CreateMul(left, right);
-            //    break;
-            //case BinaryType::Div:
-            //    this->value = this->builder->CreateSDiv(left, right);
-            //    break;
-            //case BinaryType::Mod:
-            //    this->value = this->builder->CreateSRem(left, right);
-            //    break;
-            //case BinaryType::Equal:
-            //{
-            //    auto cmp = this->builder->CreateICmpEQ(left, right);
-            //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
-            //    break;
-            //}
-            //case BinaryType::NotEqual:
-            //{
-            //    auto cmp = this->builder->CreateICmpNE(left, right);
-            //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
-            //    break;
-            //}
-            //case BinaryType::LessThan:
-            //{
-            //    auto cmp = this->builder->CreateICmpSLT(left, right);
-            //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
-            //    break;
-            //}
-            //case BinaryType::LessThanOrEqual:
-            //{
-            //    auto cmp = this->builder->CreateICmpSLE(left, right);
-            //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
-            //    break;
-            //}
-            //case BinaryType::GreaterThanOrEqual:
-            //{
-            //    auto cmp = this->builder->CreateICmpSGE(left, right);
-            //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
-            //    break;
-            //}
-            //case BinaryType::GreaterThan:
-            //{
-            //    auto cmp = this->builder->CreateICmpSGT(left, right);
-            //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
-            //    break;
-            //}
-            //default:
-            //    UNREACHABLE();
-            //    break;
-            //}
+            // First, evaluate left operand and store its result to the temporal variable
+            op.GetLeft()->Accept(*this);
+            this->builder->CreateCall(gmp->llvm_mpz_init_set, { GetValuePtr(temp), GetValuePtr() });
+
+            // Next, evaluate right operand
+            op.GetRight()->Accept(*this);
+
+            switch (op.GetType()) {
+            case BinaryType::Add:
+                this->builder->CreateCall(gmp->llvm_mpz_add, { GetValuePtr(), GetValuePtr(temp), GetValuePtr() });
+                break;
+            case BinaryType::Sub:
+                this->builder->CreateCall(gmp->llvm_mpz_sub, { GetValuePtr(), GetValuePtr(temp), GetValuePtr() });
+                break;
+            case BinaryType::Mult:
+                this->builder->CreateCall(gmp->llvm_mpz_mul, { GetValuePtr(), GetValuePtr(temp), GetValuePtr() });
+                break;
+            case BinaryType::Div:
+                this->builder->CreateCall(gmp->llvm_mpz_tdiv_q, { GetValuePtr(), GetValuePtr(temp), GetValuePtr() });
+                break;
+            case BinaryType::Mod:
+                this->builder->CreateCall(gmp->llvm_mpz_tdiv_r, { GetValuePtr(), GetValuePtr(temp), GetValuePtr() });
+                break;
+                //case BinaryType::Equal:
+                //{
+                //    auto cmp = this->builder->CreateICmpEQ(left, right);
+                //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
+                //    break;
+                //}
+                //case BinaryType::NotEqual:
+                //{
+                //    auto cmp = this->builder->CreateICmpNE(left, right);
+                //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
+                //    break;
+                //}
+                //case BinaryType::LessThan:
+                //{
+                //    auto cmp = this->builder->CreateICmpSLT(left, right);
+                //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
+                //    break;
+                //}
+                //case BinaryType::LessThanOrEqual:
+                //{
+                //    auto cmp = this->builder->CreateICmpSLE(left, right);
+                //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
+                //    break;
+                //}
+                //case BinaryType::GreaterThanOrEqual:
+                //{
+                //    auto cmp = this->builder->CreateICmpSGE(left, right);
+                //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
+                //    break;
+                //}
+                //case BinaryType::GreaterThan:
+                //{
+                //    auto cmp = this->builder->CreateICmpSGT(left, right);
+                //    this->value = this->builder->CreateSelect(cmp, this->builder->getIntN(IntegerBits<TNumber>, 1), this->builder->getIntN(IntegerBits<TNumber>, 0));
+                //    break;
+                //}
+            default:
+                UNREACHABLE();
+                break;
+            }
+
+            // Free temporal variable
+            this->builder->CreateCall(gmp->llvm_mpz_clear, { GetValuePtr(temp) });
         }
 
         virtual void Visit(const ConditionalOperator &op) override {
