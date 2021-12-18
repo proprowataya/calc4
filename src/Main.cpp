fedd6a0db338bc678638043dbabe5e072c96ab7e
Yuya Watari <8926297+proprowataya@users.noreply.github.com>
2021-12-18 23:17:30 +0900

    If the source file pathes are specified, the program executes them

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,10 +6,12 @@
 #include "Test.h"
 #include <cstdint>
 #include <ctime>
+#include <fstream>
 #include <gmpxx.h>
 #include <iostream>
 #include <limits>
 #include <sstream>
+#include <streambuf>
 
 constexpr const char* ProgramName = "Calc4 REPL";
 constexpr const int InfinitePrecisionIntegerSize = std::numeric_limits<int>::max();
@@ -44,13 +46,24 @@ constexpr const char* DisableOptimization = "-Od";
 constexpr const char* InfinitePrecisionInteger = "inf";
 }
 
+std::tuple<Option, std::vector<const char*>, bool> ParseCommandLineArgs(int argc, char** argv);
+
+template<typename TNumber>
+void Run(Option& option, const std::vector<const char*>& sources);
+
+template<typename TNumber>
+void RunSources(const Option& option, const std::vector<const char*>& sources);
+
+template<typename TNumber>
+void RunAsRepl(Option& option);
+
+template<typename TNumber>
+void ExecuteCore(const std::string& source, CompilationContext& context,
+                 ExecutionState<TNumber>& state, const Option& option);
+
 inline bool StringEquals(const char* a, const char* b);
 inline bool IsSupportedIntegerSize(int size);
 void PrintHelp(int argc, char** argv);
-
-template<typename TNumber>
-void ReplCore(Option& option);
-
 void PrintTree(const Operator& op, int depth);
 bool HasRecursiveCall(const Operator& op, const CompilationContext& context);
 bool HasRecursiveCallInternal(const Operator& op, const CompilationContext& context,
@@ -62,10 +75,53 @@ int main(int argc, char** argv)
     using namespace std;
     using namespace llvm;
 
+    /* ***** Parse command line args ***** */
+    auto [option, sources, performTest] = ParseCommandLineArgs(argc, argv);
+
+    /* ***** Initialize LLVM if needed ***** */
+    if (performTest || option.executionType == ExecutionType::JIT)
+    {
+        LLVMInitializeNativeTarget();
+        LLVMInitializeNativeAsmPrinter();
+        LLVMInitializeNativeAsmParser();
+    }
+
+    /* ***** Perform test if specified ***** */
+    if (performTest)
+    {
+        TestAll();
+        llvm_shutdown();
+        return 0;
+    }
+
+    switch (option.integerSize)
+    {
+    case 32:
+        Run<int32_t>(option, sources);
+        break;
+    case 64:
+        Run<int64_t>(option, sources);
+        break;
+    case 128:
+        Run<__int128_t>(option, sources);
+        break;
+    case InfinitePrecisionIntegerSize:
+        Run<mpz_class>(option, sources);
+        break;
+    default:
+        UNREACHABLE();
+        break;
+    }
+
+    llvm_shutdown();
+}
+
+std::tuple<Option, std::vector<const char*>, bool> ParseCommandLineArgs(int argc, char** argv)
+{
     Option option;
+    std::vector<const char*> sources;
     bool performTest = false;
 
-    /* ***** Parse command line args ***** */
     for (int i = 1; i < argc; i++)
     {
         char* str = argv[i];
@@ -86,7 +142,7 @@ int main(int argc, char** argv)
             if (StringEquals(str, CommandLineArgs::Help))
             {
                 PrintHelp(argc, argv);
-                return 0;
+                exit(0);
             }
             else if (StringEquals(str, CommandLineArgs::PerformTest))
             {
@@ -137,70 +193,179 @@ int main(int argc, char** argv)
             }
             else
             {
-                std::ostringstream oss;
-                oss << "Unknown option \"" << str << "\"";
-                throw oss.str();
+                sources.push_back(str);
             }
         }
         catch (std::string& error)
         {
-            cout << "Error: " << error << endl << endl;
+            std::cout << "Error: " << error << std::endl << std::endl;
             PrintHelp(argc, argv);
-            return EXIT_FAILURE;
+            exit(EXIT_FAILURE);
         }
     }
+
+    return std::make_tuple(option, sources, performTest);
+}
+
+template<typename TNumber>
+void Run(Option& option, const std::vector<const char*>& sources)
+{
+    if (!sources.empty())
+    {
+        /* ***** If the source files are specified, we execute them ***** */
+        RunSources<TNumber>(option, sources);
+    }
+    else
+    {
+        /* ***** Otherwise, this program behaves as repl ***** */
+        RunAsRepl<TNumber>(option);
+    }
+}
+
+template<typename TNumber>
+void RunSources(const Option& option, const std::vector<const char*>& sources)
+{
+    for (auto path : sources)
+    {
+        std::ifstream ifs(path);
+        if (!ifs)
+        {
+            std::cerr << "Error: Could not open \"" << path << "\"" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        std::string source = { std::istreambuf_iterator<char>(ifs),
+                               std::istreambuf_iterator<char>() };
+
+        CompilationContext context;
+        ExecutionState<TNumber> state;
+        ExecuteCore(source, context, state, option);
+    }
+}
+
+template<typename TNumber>
+void RunAsRepl(Option& option)
+{
+    using namespace std;
 
     /* ***** Print header ***** */
     cout << ProgramName << endl;
 
     /* ***** Print current setting ***** */
-    if (!performTest)
+    cout << "    Integer size: "
+         << (option.integerSize == InfinitePrecisionIntegerSize
+                 ? "Infinite-precision"
+                 : std::to_string(option.integerSize))
+         << endl
+         << "    Executor: " << GetExecutionTypeString(option.executionType) << endl
+         << "    Always JIT: " << (option.alwaysJit ? "on" : "off") << endl
+         << "    Optimize: " << (option.optimize ? "on" : "off") << endl
+         << endl;
+
+    CompilationContext context;
+    ExecutionState<TNumber> state;
+
+    while (true)
     {
-        cout << "    Integer size: "
-             << (option.integerSize == InfinitePrecisionIntegerSize
-                     ? "Infinite-precision"
-                     : std::to_string(option.integerSize))
-             << endl
-             << "    Executor: " << GetExecutionTypeString(option.executionType) << endl
-             << "    Always JIT: " << (option.alwaysJit ? "on" : "off") << endl
-             << "    Optimize: " << (option.optimize ? "on" : "off") << endl
+        string line;
+        cout << "> ";
+        getline(cin, line);
+
+        if (line == "#print on")
+        {
+            option.printInfo = true;
+            cout << endl;
+            continue;
+        }
+        else if (line == "#print off")
+        {
+            option.printInfo = false;
+            cout << endl;
+            continue;
+        }
+        else if (line == "#optimize on")
+        {
+            option.optimize = true;
+            cout << endl;
+            continue;
+        }
+        else if (line == "#optimize off")
+        {
+            option.optimize = false;
+            cout << endl;
+            continue;
+        }
+
+        ExecuteCore<TNumber>(line, context, state, option);
+    }
+}
+
+template<typename TNumber>
+void ExecuteCore(const std::string& source, CompilationContext& context,
+                 ExecutionState<TNumber>& state, const Option& option)
+{
+    using namespace std;
+    using namespace llvm;
+
+    try
+    {
+        auto tokens = Lex(source, context);
+        auto op = Parse(tokens, context);
+        if (option.optimize)
+        {
+            op = Optimize<TNumber>(context, op);
+        }
+
+        bool hasRecursiveCall = HasRecursiveCall(*op, context);
+
+        if (option.printInfo)
+        {
+            cout << "Has recursive call: " << (hasRecursiveCall ? "True" : "False") << endl;
+
+            cout << "Tree:" << endl
+                 << "---------------------------" << endl
+                 << "Module {" << endl
+                 << "Main:" << endl;
+            PrintTree(*op, 1);
+            cout << endl;
+
+            for (auto it = context.UserDefinedOperatorBegin();
+                 it != context.UserDefinedOperatorEnd(); it++)
+            {
+                auto& name = it->second.GetDefinition().GetName();
+                auto& op = it->second.GetOperator();
+
+                cout << name << ":" << endl;
+                PrintTree(*op, 1);
+            }
+
+            cout << "}" << endl << "---------------------------" << endl << endl;
+        }
+
+        TNumber result;
+        clock_t start = clock();
+        if constexpr (std::is_same_v<TNumber, mpz_class>)
+        {
+            // Jit compiler does not support GMP
+            result = Evaluate<TNumber>(context, state, op);
+        }
+        else
+        {
+            result = (option.executionType == ExecutionType::JIT &&
+                      (option.alwaysJit || HasRecursiveCall(*op, context)))
+                ? EvaluateByJIT<TNumber>(context, state, op, option.optimize, option.printInfo)
+                : Evaluate<TNumber>(context, state, op);
+        }
+        clock_t end = clock();
+
+        cout << result << endl
+             << "Elapsed: " << (double)(end - start) / (CLOCKS_PER_SEC / 1000.0) << " ms" << endl
              << endl;
     }
-
-    /* ***** Initialize LLVM if needed ***** */
-    if (performTest || option.executionType == ExecutionType::JIT)
+    catch (std::string& error)
     {
-        LLVMInitializeNativeTarget();
-        LLVMInitializeNativeAsmPrinter();
-        LLVMInitializeNativeAsmParser();
+        cout << "Error: " << error << endl << endl;
     }
-
-    if (performTest)
-    {
-        TestAll();
-        return 0;
-    }
-
-    switch (option.integerSize)
-    {
-    case 32:
-        ReplCore<int32_t>(option);
-        break;
-    case 64:
-        ReplCore<int64_t>(option);
-        break;
-    case 128:
-        ReplCore<__int128_t>(option);
-        break;
-    case InfinitePrecisionIntegerSize:
-        ReplCore<mpz_class>(option);
-        break;
-    default:
-        UNREACHABLE();
-        break;
-    }
-
-    llvm_shutdown();
 }
 
 inline bool StringEquals(const char* a, const char* b)
@@ -247,109 +412,6 @@ void PrintHelp(int argc, char** argv)
          << Indent << "Disable optimization" << endl
          << CommandLineArgs::PerformTest << endl
          << Indent << "Perform test" << endl;
-}
-
-template<typename TNumber>
-void ReplCore(Option& option)
-{
-    using namespace std;
-    using namespace llvm;
-
-    CompilationContext context;
-    ExecutionState<TNumber> state;
-
-    while (true)
-    {
-        try
-        {
-            string line;
-            cout << "> ";
-            getline(cin, line);
-
-            if (line == "#print on")
-            {
-                option.printInfo = true;
-                cout << endl;
-                continue;
-            }
-            else if (line == "#print off")
-            {
-                option.printInfo = false;
-                cout << endl;
-                continue;
-            }
-            else if (line == "#optimize on")
-            {
-                option.optimize = true;
-                cout << endl;
-                continue;
-            }
-            else if (line == "#optimize off")
-            {
-                option.optimize = false;
-                cout << endl;
-                continue;
-            }
-
-            auto tokens = Lex(line, context);
-            auto op = Parse(tokens, context);
-            if (option.optimize)
-            {
-                op = Optimize<TNumber>(context, op);
-            }
-
-            bool hasRecursiveCall = HasRecursiveCall(*op, context);
-
-            if (option.printInfo)
-            {
-                cout << "Has recursive call: " << (hasRecursiveCall ? "True" : "False") << endl;
-
-                cout << "Tree:" << endl
-                     << "---------------------------" << endl
-                     << "Module {" << endl
-                     << "Main:" << endl;
-                PrintTree(*op, 1);
-                cout << endl;
-
-                for (auto it = context.UserDefinedOperatorBegin();
-                     it != context.UserDefinedOperatorEnd(); it++)
-                {
-                    auto& name = it->second.GetDefinition().GetName();
-                    auto& op = it->second.GetOperator();
-
-                    cout << name << ":" << endl;
-                    PrintTree(*op, 1);
-                }
-
-                cout << "}" << endl << "---------------------------" << endl << endl;
-            }
-
-            TNumber result;
-            clock_t start = clock();
-            if constexpr (std::is_same_v<TNumber, mpz_class>)
-            {
-                // Jit compiler does not support GMP
-                result = Evaluate<TNumber>(context, state, op);
-            }
-            else
-            {
-                result = (option.executionType == ExecutionType::JIT &&
-                          (option.alwaysJit || HasRecursiveCall(*op, context)))
-                    ? EvaluateByJIT<TNumber>(context, state, op, option.optimize, option.printInfo)
-                    : Evaluate<TNumber>(context, state, op);
-            }
-            clock_t end = clock();
-
-            cout << result << endl
-                 << "Elapsed: " << (double)(end - start) / (CLOCKS_PER_SEC / 1000.0) << " ms"
-                 << endl
-                 << endl;
-        }
-        catch (std::string& error)
-        {
-            cout << "Error: " << error << endl << endl;
-        }
-    }
 }
 
 void PrintTree(const Operator& op, int depth)
