63127229a3d2f571044d73d309ec3a71302af374
Yuya Watari <8926297+proprowataya@users.noreply.github.com>
2022-01-30 17:55:24 +0900

    Add a new unit test

---
+++ test/TestMain.cpp
@@ -0,0 +1,37 @@
+/*****
+ *
+ * The Calc4 Programming Language
+ *
+ * Copyright (C) 2018-2022 Yuya Watari
+ * This software is released under the MIT License, see LICENSE file for details
+ *
+ *****/
+
+#include "TestCommon.h"
+#include <gtest/gtest.h>
+
+class Environment : public ::testing::Environment
+{
+public:
+    ~Environment() override {}
+
+    // Override this to define how to set up the environment.
+    void SetUp() override
+    {
+#ifdef ENABLE_JIT
+        LLVMInitializeNativeTarget();
+        LLVMInitializeNativeAsmPrinter();
+        LLVMInitializeNativeAsmParser();
+#endif // ENABLE_JIT
+    }
+
+    // Override this to define how to tear down the environment.
+    void TearDown() override
+    {
+#ifdef ENABLE_JIT
+        llvm::llvm_shutdown();
+#endif // ENABLE_JIT
+    }
+};
+
+::testing::Environment* const environment = testing::AddGlobalTestEnvironment(new Environment);
